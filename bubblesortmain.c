b3096dd15872a8357343d84556b9f922edc25328
vguerra18 <100383778+vguerra18@users.noreply.github.com>
2022-08-31 00:23:50 -0300

    TRABALHO 1

---
+++ bubblesortmain.c
@@ -0,0 +1,18 @@
+#include "TAD_sort.h"
+int main()
+{
+    double time_spent = 0.0;
+    clock_t begin = clock();
+    long int tamanho;
+    printf("\nTamanho: ");
+    scanf("%ld", &tamanho);
+    Float_Vector *vetor = alocavetor(tamanho);
+
+    bubble(vetor, tamanho);
+
+    clock_t end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("\n%f seconds", time_spent);
+    free(vetor);
+    return 0;
+}
